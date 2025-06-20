4699b77ae67d5ea0d9e41c34c6a24c1e1694e697
Nihal Rawat <nihalroyals.03@gmail.com>
2025-06-20 17:56:57 +0530

    Update CORS logic

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ import nodemailer from "nodemailer";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "https://item-management-dusky.vercel.app",
+  methods: ["GET", "POST"],
+}));
 app.use(express.json({ limit: '10mb' }));
 
 mongoose.connect(process.env.MONGO_URL).then(() => console.log("MongoDB connected"))
